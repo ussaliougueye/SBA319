0074f372ab19503c0b68afe09995cfcfc19d8054
ussaliougueye <sn.saliougueye@gmail.com>
2025-06-24 08:32:30 -0400

    update

--- models/publications.js
+++ models/publications.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const publicationSchema = new Schema({
-  // Basic fields
   title: {
     type: String,
     required: [true, 'Title is required'],
@@ -18,12 +17,7 @@ const publicationSchema = new Schema({
     default: ''
   },
 
-  // Author reference (like a foreign key)
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',  // Links to the 'User' collection
-    required: true
-  },
+  
 
   // Timestamps
   createdAt: {
@@ -34,25 +28,16 @@ const publicationSchema = new Schema({
     type: Date,
     default: Date.now
   },
-
-  // Interactions
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'  // Array of user IDs who liked the post
-  }],
-  comments: [{
+  likes: {
+    type: Number
+  },
+  comments: {
     text: String,
-    postedBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
-    },
     createdAt: {
       type: Date,
       default: Date.now
     }
-  }],
-
-  
+  },
   isPublished: {
     type: Boolean,
     default: true
@@ -61,7 +46,7 @@ const publicationSchema = new Schema({
 
 
 
-// Create text index for search (e.g., on 'title' and 'content')
+
 publicationSchema.index({ title: 'text', content: 'text' });
 
 module.exports = mongoose.model('Publication', publicationSchema);
